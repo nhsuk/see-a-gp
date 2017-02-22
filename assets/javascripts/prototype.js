354f19451f4cad23f9a913f005845347bdf5b560
Danielle Mullis <danielle.nhs@Danielles-MacBook-Air.local>
2017-02-22 14:26:30 +0000

    make medicines cards expand with css animation

--- assets/javascripts/prototype.js
+++ assets/javascripts/prototype.js
@@ -13,15 +13,33 @@ $(document).ready(function(){
     
     var getChildren = $(this).parents(".card").children();
     
+    var totalHeight = 0;
+    
     $(getChildren).each(function() {
       
-      var getHeight = $(this).height();
+      var getHeight = $(this).outerHeight();
       
-      console.log(getHeight);
+      totalHeight += getHeight;
+      
+      console.log(totalHeight);
       
     });
     
-    console.log(getChildren);
+    if ( $(this).parents(".card").hasClass("is-expanded") == true ) {
+      
+      $(this).parents(".card").css("height", (totalHeight - 76));
+      $(this).parents(".card").find(".card__toggle-btn").text("Less information");
+      
+    } else if ( $(this).parents(".card").hasClass("is-expanded") == false ) {
+      
+      $(this).parents(".card").css("height", "400px");
+      $(this).parents(".card").find(".card__toggle-btn").text("More information");
+      
+    }
+    
+    
+    
+    //console.log(getChildren);
 
   });
   
--- build/assets/javascripts/nhsuk.bundle.js
+++ build/assets/javascripts/nhsuk.bundle.js
@@ -11851,14 +11851,28 @@
 	
 	    var getChildren = $(this).parents(".card").children();
 	
+	    var totalHeight = 0;
+	
 	    $(getChildren).each(function () {
 	
-	      var getHeight = $(this).height();
+	      var getHeight = $(this).outerHeight();
 	
-	      console.log(getHeight);
+	      totalHeight += getHeight;
+	
+	      console.log(totalHeight);
 	    });
 	
-	    console.log(getChildren);
+	    if ($(this).parents(".card").hasClass("is-expanded") == true) {
+	
+	      $(this).parents(".card").css("height", totalHeight - 76);
+	      $(this).parents(".card").find(".card__toggle-btn").text("Less information");
+	    } else if ($(this).parents(".card").hasClass("is-expanded") == false) {
+	
+	      $(this).parents(".card").css("height", "400px");
+	      $(this).parents(".card").find(".card__toggle-btn").text("More information");
+	    }
+	
+	    //console.log(getChildren);
 	  });
 	
 	  // Toggle expand and collapsed states from button
